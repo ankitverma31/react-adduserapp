d89878facc938b75a2a6a0ad13ebddf1c1e0d1d2
Ankit Verma <ankit.v@media.net>
2022-04-17 21:47:57 +0530

    build folder added

---
+++ build/precache-manifest.11c98cf80a88dba78d9e8e8247cb2f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44de4d8e22da4bea7b4d30c2ca6a3608",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c4305c321d00a4d2078",
+    "url": "/static/css/main.67a9d557.chunk.css"
+  },
+  {
+    "revision": "41d7a40c1f16eda7739f",
+    "url": "/static/js/2.de5cd2e8.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.de5cd2e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4305c321d00a4d2078",
+    "url": "/static/js/main.e5833e98.chunk.js"
+  },
+  {
+    "revision": "f13d5fb20258b992088c",
+    "url": "/static/js/runtime-main.a6b2f021.js"
+  }
+]);
